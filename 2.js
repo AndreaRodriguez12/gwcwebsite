89a308245491c392723a1e8c8def7bff38147ec0
AndreaRodriguez12 <41123991+AndreaRodriguez12@users.noreply.github.com>
2018-07-11 11:09:16 -0700

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,7 @@
+// this is your javascript file
+function clickbutton(){
+  document.getElementById('demo').innerHTML = Date();
+  demo.style.color = 'white';
+  demo.style.fontFamily = 'Comic Sans MS';
+  demo.style.fontSize = '50';
+}
